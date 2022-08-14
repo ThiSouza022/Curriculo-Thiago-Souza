a119d38be84b813db26ab67bf195b3dd6648960f
Thiago Souza <thisouza.dev@gmail.com>
2022-08-14 02:21:25 -0300

    visual updates

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 const target = document.querySelectorAll('[data-anime]');
 
 
-function animeScroll () {
-    const windowTop= window.pageYOffset;
-    target.forEach(function(e){
-        if((windowTop) > 400){
+function animeScroll() {
+    const windowTop = window.pageYOffset;
+    target.forEach(function (e) {
+        if ((windowTop) > 400) {
             e.classList.add('animate');
             e.classList.remove('animateoff');
             e.classList.remove('hidden');
         }
-        else{
+        else {
             e.classList.add('animateoff');
-            e.classList.remove('animate');   
+            e.classList.remove('animate');
         }
     })
 }
 
-animeScroll ();
+animeScroll();
 
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', function () {
     animeScroll();
 })
